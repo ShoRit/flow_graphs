ebadfe58905fb75dd481dd2b59fabbe7690fb890
Sireesh Gururaja <sgururaj@cs.cmu.edu>
2022-07-14 17:19:53 -0400

    update gnn depth to 4

--- code/train_single_dataset.sh
+++ code/train_single_dataset.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 for (( SEED = 0 ; SEED <= 2; SEED++ ))
 do
-    python rel_classification_info.py --src_dataset $1 --tgt_dataset $1 --mode train --domain src --dep 1 --amr 1 --gnn rgcn --seed $SEED --alpha 1.0 --batch_size 16 --gnn_depth 2 --gpu 0 --lr 2e-5
+    python rel_classification_info.py --src_dataset $1 --tgt_dataset $1 --mode train --domain src --dep 1 --amr 1 --gnn rgcn --seed $SEED --alpha 1.0 --batch_size 16 --gnn_depth 4 --gpu 0 --lr 2e-5
 done
