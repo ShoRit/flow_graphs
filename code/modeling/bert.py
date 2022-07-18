dea9a9319916ad541399c8312667b570ba9c6685
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2022-07-18 16:46:57 -0400

    Remove no-graph variant: we can decide to put this back in, but it adds complexity for now.

--- code/modeling/bert.py
+++ code/modeling/bert.py
@@ -6,76 +6,8 @@ from transformers import BertModel, BertPreTrainedModel
 from modeling.graph_components import DeepNet
 
 
-class BertRelationClassifier(BertPreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.num_labels = config.num_labels
-        self.relation_emb_dim = config.relation_emb_dim
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.fclayer = nn.Linear(config.hidden_size * 3, self.relation_emb_dim)
-        self.classifier = nn.Linear(self.relation_emb_dim, self.config.num_labels)
-        self.loss_fn = nn.CrossEntropyLoss
-        self.init_weights()
-
-    def forward(
-        self,
-        input_ids=None,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        e1_mask=None,
-        e2_mask=None,
-        head_mask=None,
-        inputs_embeds=None,
-        input_relation_emb=None,
-        labels=None,
-        device="cpu",  # this should be an instantiation parameter, it will fail if you change
-        # device after instantiation.
-    ):
-        output_dict = {}
-
-        bert_outputs = self.bert(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            head_mask=head_mask,
-            inputs_embeds=inputs_embeds,
-        )
-
-        sequence_output = bert_outputs["last_hidden_state"]  # Sequence of hidden-states of the last
-        # layer.
-        pooled_output = bert_outputs["pooler_output"]  # Last layer hidden-state of the [CLS] token
-        # further processed by a Linear layer and a Tanh activation function.
-
-        def extract_entity(sequence_output, e_mask):
-            extended_e_mask = e_mask.unsqueeze(1)
-            extended_e_mask = torch.bmm(extended_e_mask.float(), sequence_output).squeeze(1)
-            return extended_e_mask.float()
-
-        e1_h = extract_entity(sequence_output, e1_mask)
-        e2_h = extract_entity(sequence_output, e2_mask)
-        context = self.dropout(pooled_output)
-        pooled_output = torch.cat([context, e1_h, e2_h], dim=-1)
-        pooled_output = torch.tanh(pooled_output)  # why tanh?
-        pooled_output = self.fclayer(pooled_output)
-
-        relation_embeddings = torch.tanh(pooled_output)
-        output_dict["relation_embeddings"] = relation_embeddings
-
-        logits = self.classifier(relation_embeddings)  # [batch_size x hidden_size]
-        output_dict["logits"] = logits
-
-        if labels is not None:
-            loss = self.loss_fn(logits.view(-1, self.num_labels), labels.view(-1))
-            output_dict["loss"] = loss
-
-        return output_dict
-
-
 class BertRGCNRelationClassifier(BertPreTrainedModel):
-    def __init__(self, config):
+    def __init__(self, config, use_graph_data):
         super().__init__(config)
         self.num_labels = config.num_labels
         self.relation_emb_dim = config.relation_emb_dim
@@ -90,6 +22,7 @@ class BertRGCNRelationClassifier(BertPreTrainedModel):
         self.fc_layer = nn.Linear(rel_in, self.relation_emb_dim)
         self.rel_classifier = nn.Linear(self.relation_emb_dim, self.config.num_labels)
         self.config = config
+        self.use_graph_data = use_graph_data
         self.init_weights()
 
     def extract_entity(self, sequence_output, e_mask):
@@ -132,37 +65,38 @@ class BertRGCNRelationClassifier(BertPreTrainedModel):
 
         rel_output = [context, e1_h, e2_h]
 
-        n1_mask, n2_mask, batch = (
-            graph_data.n1_mask,
-            graph_data.n2_mask,
-            graph_data.batch,
-        )
-        batch_np = batch.cpu().detach().numpy()
-        graph_embs = []
-        for idx in range(0, sequence_output.shape[0]):
-            bids = np.where(batch_np == idx)[0]
-            sid, eid = bids[0], bids[-1] + 1
-            graph_embs.append(
-                torch.max(sequence_output[idx] + graph_data.x[sid:eid, :, None], dim=1)[0]
+        if self.use_graph_data:
+            n1_mask, n2_mask, batch = (
+                graph_data.n1_mask,
+                graph_data.n2_mask,
+                graph_data.batch,
             )
+            batch_np = batch.cpu().detach().numpy()
+            graph_embs = []
+            for idx in range(0, sequence_output.shape[0]):
+                bids = np.where(batch_np == idx)[0]
+                sid, eid = bids[0], bids[-1] + 1
+                graph_embs.append(
+                    torch.max(sequence_output[idx] + graph_data.x[sid:eid, :, None], dim=1)[0]
+                )
 
-        graph_embs = torch.vstack(graph_embs)
-        graph_embs[graph_embs == -torch.inf] = 0
+            graph_embs = torch.vstack(graph_embs)
+            graph_embs[graph_embs == -torch.inf] = 0
 
-        graph_embs = self.gnn(graph_embs, graph_data.edge_index, graph_data.edge_type)
+            graph_embs = self.gnn(graph_embs, graph_data.edge_index, graph_data.edge_type)
 
-        e1_dep, e2_dep = [], []
-        for idx in range(0, sequence_output.shape[0]):
-            mask = torch.where(batch == idx, 1, 0)
-            m1, m2 = mask * n1_mask, mask * n2_mask
-            e1_dep.append(torch.mm(m1.unsqueeze(dim=0).float(), graph_embs))
-            e2_dep.append(torch.mm(m2.unsqueeze(dim=0).float(), graph_embs))
+            e1_dep, e2_dep = [], []
+            for idx in range(0, sequence_output.shape[0]):
+                mask = torch.where(batch == idx, 1, 0)
+                m1, m2 = mask * n1_mask, mask * n2_mask
+                e1_dep.append(torch.mm(m1.unsqueeze(dim=0).float(), graph_embs))
+                e2_dep.append(torch.mm(m2.unsqueeze(dim=0).float(), graph_embs))
 
-        e1_dep = torch.cat(e1_dep, dim=0)
-        e2_dep = torch.cat(e2_dep, dim=0)
+            e1_dep = torch.cat(e1_dep, dim=0)
+            e2_dep = torch.cat(e2_dep, dim=0)
 
-        rel_output.append(e1_dep)
-        rel_output.append(e2_dep)
+            rel_output.append(e1_dep)
+            rel_output.append(e2_dep)
 
         pooled_output = torch.cat(rel_output, dim=-1)
 
@@ -180,7 +114,7 @@ class BertRGCNRelationClassifier(BertPreTrainedModel):
             output_dict["logits"] = logits
 
             ce_loss = nn.CrossEntropyLoss()
-            loss = (ce_loss(logits.view(-1, self.num_labels), labels.view(-1))) * self.alpha
+            loss = ce_loss(logits.view(-1, self.num_labels), labels.view(-1))
             output_dict["loss"] = loss
 
         return output_dict
