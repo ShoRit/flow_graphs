7f12fdb21da7ce364c2ca433bc2a709c34513199
Sireesh Gururaja <sgururaj@cs.cmu.edu>
2022-07-18 21:32:42 -0400

    rearrange script to get nicer viz

--- code/train_single_dataset.sh
+++ code/train_single_dataset.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-for (( DEP=0 ; DEP <= 1; DEP++ ))
+for (( SEED = 0 ; SEED <= 2; SEED++ ))
 do
-    for (( AMR=0 ; AMR <= 1; AMR++ ))
+    for (( DEP=0 ; DEP <= 1; DEP++ ))
     do
-        for (( SEED = 0 ; SEED <= 2; SEED++ ))
+        for (( AMR=0 ; AMR <= 1; AMR++ ))
         do
             python rel_classification_info.py --src_dataset $1 --tgt_dataset $1 --mode train --domain src --dep $DEP --amr $AMR --gnn rgcn --seed $SEED --batch_size 16 --gnn_depth 4 --gpu 0 --lr 2e-5
         done
