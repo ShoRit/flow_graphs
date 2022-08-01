783dbf31d020c77486ae35921eca0de50f658939
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2022-08-01 18:38:06 -0400

    Update training script

--- code/train_single_dataset.sh
+++ code/train_single_dataset.sh
@@ -6,11 +6,8 @@ RAYON_RS_NUM_CPUS=5
 
 for (( SEED = 0 ; SEED <= 2; SEED++ ))
 do
-    for (( DEP=0 ; DEP <= 1; DEP++ ))
+    for VALUE in dep amr
     do
-        for (( AMR=0 ; AMR <= 1; AMR++ ))
-        do
-            python rel_classification_info.py --src_dataset $1 --tgt_dataset $1 --mode train --domain src --dep $DEP --amr $AMR --gnn rgcn --seed $SEED --batch_size 16 --gnn_depth 4 --gpu 0 --lr 2e-5
-        done
+            echo python train_single_domain_model.py --dataset risec --seed $SEED --experiment_config "$VALUE"_residual --gpu None
     done
 done
