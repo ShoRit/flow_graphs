72dbe36fcc77f64324875efc7b2ee521679c6db9
Sireesh Gururaja <sgururaj@cs.cmu.edu>
2022-11-02 13:27:02 -0400

    minor bug fixes to parameter passing

--- code/train_all_indomain.sh
+++ code/train_all_indomain.sh
@@ -7,5 +7,5 @@ EXPERIMENT_CONFIG=amr_residual
 
 for SEED in 0 1 2
 do
-    sbatch --partition=shire_general --gres=gpu:A4500:1 ./singularity_runner.sh ./train_single_indomain_model.sh --dataset $DATASET --seed $SEED --experiment_config $EXPERIMENT_CONFIG 
+    srun --pty --partition=shire_general --gres=gpu:A4500:1 ./singularity_runner.sh ./train_single_indomain_model.sh  $DATASET $SEED $EXPERIMENT_CONFIG 
 done
